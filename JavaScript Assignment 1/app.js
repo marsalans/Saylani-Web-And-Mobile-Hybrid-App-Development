b3da797a5e1673c3f5681056b22c6550c41ef2bd
Muhammad Arsalan Siddiqui <muhammadarsalansiddiqui@hotmail.com>
2020-06-14 16:54:12 +0500

    Add files via upload

---
+++ JavaScript Assignment 1/app.js
@@ -0,0 +1,110 @@
+/* CHAPTER1 */
+
+
+//1
+//alert("Welcome Visitor");
+
+//2
+//alert("Error! Please enter a vaid password.");
+
+//3
+//alert("Welcome to JSLand... \n Happy Coding!");
+
+//4
+//alert("Welcome to JSLand");
+//alert("Happy Coding!");
+
+//5
+//console.log(alert("Hello... I can run JS through my browser's console"));
+
+
+
+
+
+
+/*CHAPTER2*/
+
+//1
+//var username="arsalan";
+
+//2
+//var myName="Muhammad Arsalan Siddiqui";
+
+//3a
+//var message;
+//3b
+//message="Hello World";
+//3c
+//alert(message);
+
+//4
+//var stuName=prompt("Please enter your name");
+//var stuAge=prompt("Please enter your age");
+//var stuCourse=prompt("Please enter your course");
+//alert(stuName);
+//alert(stuAge+" years old");
+//alert(stuCourse);
+
+//5
+
+//6
+//var email="mailofarsalan@gmail.com";
+//alert("My email address is "+email);
+
+//7
+//var book="“A smarter way to learn JavaScript";
+//alert("I am trying to learn from Book A smarter way to learn JavaScript" +book);
+
+//8
+//document.write("Yah! I can write HTML content trhough JavaScript");
+
+//9
+//var phulPatti="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(phulPatti);
+
+
+
+
+
+
+/* CHAPTER3 */
+
+//1
+//var age="23";
+//alert("I am "+age+" years old");
+
+//2
+
+
+//3
+//var birthYear=1997;
+//document.write("My birth year is "+birthYear+"<br>");
+//document.write("Data type of my declared variable is "+(typeof birthYear) );
+
+//4a
+//var visName=prompt("Enter Name");
+//4b
+//var prodTitle=prompt("Enter Product Title");
+//4c
+//var prodQunatity=prompt("Enter Product Quantity");
+//document.write(visName+" ordered "+prodQunatity+" "+prodTitle+"(s) on XYZ Clothing store");
+
+
+
+
+
+
+/* CHAPTER4 */
+
+//1
+//var varOne, varTwo, varThree;
+
+//2
+//legal
+//var legal, fabr1c, g00d, under_score, karach1;
+//illegal
+//var !llegal, 1llegal, 786, $refsf, 7&g;
+
+//3a
+document.write("<h3>Rules for naming JS variables</h3><br><br>");
+//3b
