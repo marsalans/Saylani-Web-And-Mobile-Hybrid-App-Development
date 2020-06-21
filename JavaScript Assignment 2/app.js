b52d159583a45b158771e9c00f1bbeff980af49c
Muhammad Arsalan Siddiqui <muhammadarsalansiddiqui@hotmail.com>
2020-06-21 15:57:14 +0500

    Add files via upload

---
+++ JavaScript Assignment 2/app.js
@@ -0,0 +1,52 @@
+/* Chapter 21 - 25*/
+
+
+
+//1
+//var pFirstNAme=prompt("Enter First Name");
+//var pLastNAme=prompt("Enter Last Name");
+//var oFullName=pFirstNAme+' '+pLastNAme;
+//alert(oFullName);
+
+
+
+//2
+//var pFavPhone= prompt("Enter your favourite mobile phone");
+//document.write("My favourite phone is: "+pFavPhone);
+//document.write("<br> Length of string: "+pFavPhone.length);
+
+
+
+//3
+//var str1="Pakistani";
+//var strIndx="n";
+//document.write("String: "+str1);
+//document.write("<br> Index of "+strIndx+": "+ str1.indexOf(strIndx));
+
+
+
+//4
+//var str2="Hello World";
+//var strLstIndxFnd="l";
+//document.write("String: "+str2);
+//document.write("<br> Last Index of "+strLstIndxFnd+": "+ str2.lastIndexOf(strLstIndxFnd));
+
+
+
+//5
+//var str3="Pakistani";
+//var strLoc="3";
+//document.write("String: "+str3);
+//document.write("<br> Character at Index "+strLoc+": "+ str3.charAt(strLoc));
+
+
+
+//6
+var pFirstNAme=prompt("Enter First Name");
+var pLastNAme=prompt("Enter Last Name");
+var oFullName=pFirstNAme.concat(pLastNAme);
+alert(oFullName);
+
+
+
+//7
